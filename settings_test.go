742affb2574f0a3fea99b746f7979a52fb55a6cb
Andrea Franz <andrea@gravityblast.com>
2014-02-04 17:16:08 +0000

    testing host and port

--- settings_test.go
+++ settings_test.go
@@ -14,3 +14,21 @@ func TestPublicPath(t *testing.T) {
 	InitSettings()
 	assert.Equal(t, "./foo", PublicPath())
 }
+
+func TestHost(t *testing.T) {
+	InitSettings()
+	assert.Equal(t, DefaultHost, Host())
+
+	os.Setenv("HOST", "1.2.3.4")
+	InitSettings()
+	assert.Equal(t, "1.2.3.4", Host())
+}
+
+func TestPort(t *testing.T) {
+	InitSettings()
+	assert.Equal(t, DefaultPort, Port())
+
+	os.Setenv("PORT", "12345")
+	InitSettings()
+	assert.Equal(t, "12345", Port())
+}
